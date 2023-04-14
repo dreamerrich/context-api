62cf0a6e201740afc3da5614de2cbfe5f3c51f92
richi <richidhimar45@gmail.com>
2023-04-14 15:59:23 +0530

    context api example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, createContext, useContext } from "react"
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const languages = ["JavaScript", "Python"]
+const LanguageContext = createContext({
+    languages,
+    language: languages[0],
+    setLanguage: () => {},
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [language, setLanguage] = useState(languages[0])
+    return (
+        <LanguageContext.Provider value={{ languages, language, setLanguage }}>
+            <MainSection />
+        </LanguageContext.Provider>
+    )
+}
+
+function MainSection() {
+    const { languages, language, setLanguage } = useContext(LanguageContext)
+    const currentIndex = languages.indexOf(language)
+    const toggleLanguage = () => {
+        if (currentIndex === languages.length - 1) {
+            setLanguage(languages[0])
+        } else {
+            setLanguage(languages[currentIndex + 1])
+        }
+    }
+    return (
+        <div>
+            <h2 id="favoriteLanguage">{`Favorite programing language: ${language}`}</h2> &nbsp; &nbsp;
+            <button id="changeFavorite" onClick={toggleLanguage}>
+                Toggle language
+            </button>
+        </div>
+    )
 }
 
 export default App;
+ReactDOM.render(<App />, document.getElementById("root"))
